9442799cdfba80d0a981293e027f6144dad4557d
AdilaIzal <94153826+AdilaIzal@users.noreply.github.com>
2021-11-12 14:16:32 +0800

    Update Random Password Generator.py

--- Random Password Generator.py
+++ Random Password Generator.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[11]:
-
-
 import random
 passl = int(input("Please enter the length of password: "))
 s = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM123456789!@#$%&6*_/"
